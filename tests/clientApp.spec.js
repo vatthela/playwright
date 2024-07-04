301db169e0b7e9bff3b47d91197cf588998d10e5
vatthela <tunglambtl90@gmail.com>
2024-07-05 00:14:13 +0700

    commit

--- tests/clientApp.spec.js
+++ tests/clientApp.spec.js
@@ -41,9 +41,20 @@ test.only ('Page Client App Login' , async ({page}) =>{
     }
     await expect(page.locator('.user__name [type="text"]').first()).toContainText("tunglam")
     await page.locator('.action__submit').click()
-    await expect (page.locator('.hero-primary')).toContainText("THANKYOU FOR THE ORDER")
+    await expect (page.locator('.hero-primary')).toContainText("Thankyou for the order")
     const orderId = await page.locator('.em-spacer-1 .ng-star-inserted').textContent()
-    page.getByLabel
     console.log(orderId)
+    await page.locator('button[routerlink="/dashboard/myorders"]').click()
+    await page.locator('tbody').waitFor()
+    const rows = page.locator('tbody tr')
+    for (let i = 0; i < await rows.count(); i++) {
+        const rowOrderId = await rows.nth(i).locator('th').textContent()
+        if (orderId.includes(rowOrderId)) {
+            await rows.nth(i).locator('.btn-primary').first().click()
+        }
+    } 
+    const orderIdDetails = await page.locator('.col-text').textContent()
+    expect (orderId.includes(orderIdDetails)).toBeTruthy()
     await page.pause()
+
 });
