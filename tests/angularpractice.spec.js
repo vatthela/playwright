dca7e58951b7e25325e9a86080d9010dc32514f2
lam.bui <lam.bui@onemount.com>
2024-07-05 16:03:07 +0700

    commit

--- tests/angularpractice.spec.js
+++ tests/angularpractice.spec.js
@@ -5,6 +5,8 @@ test.only ('Special Locator', async({page}) => {
     await page.getByLabel('Check me out if you Love IceCreams!').check()
     await page.getByLabel('Gender').selectOption('Female')
     await page.getByLabel('Employed').check()
-    
+    await page.getByPlaceholder('Password').fill('Moihoc!1')
+    await page.getByRole("button", {name:'Submit'}).click()
+    await page.getByText(' The Form has been submitted successfully!.').isVisible()
     await page.pause()
 })
